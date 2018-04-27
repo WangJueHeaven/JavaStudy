c537ae56029ed848f219b2fd0cd8cdf87eeab976
WangJueHeaven <37130153+WangJueHeaven@users.noreply.github.com>
2018-04-27 17:33:25 +0800

    Enum add
    枚举类型的练习添加

--- study/src/com/study/chapter5/enums/EnumTest.java
+++ study/src/com/study/chapter5/enums/EnumTest.java
@@ -1,4 +1,30 @@
 package com.study.chapter5.enums;
 
+import java.util.Scanner;
+
 public class EnumTest {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a size: (SMALL, MEDIUM, LARGE, EXTRA_LARGE)");
+        //输入size同时将输入的内容转换为大写
+        String input = in.next().toUpperCase();
+        //通过toString的逆方法，由输入的String来获取枚举型常量
+        Size size = Enum.valueOf(Size.class, input);
+        System.out.println("Size=" + size);
+        System.out.println("abbreviation=" + size.getAbbreviation());
+        if(size == Size.EXTRA_LARGE)
+            System.out.println("Good job---you paid attention to the _.");
+    }
+}
+
+@SuppressWarnings("ALL")
+enum Size{
+    //枚举构造
+    SMALL("S"), MEDIUM("M"), LARGE("L"),EXTRA_LARGE("XL");
+    //构造
+    private Size(String abbreviation){this.abbreviation = abbreviation; }
+    //get
+    public String getAbbreviation(){return abbreviation;}
+    //域名
+    private String abbreviation;
 }
